1c9050b68823efe7c1f469eb7a83d597832e4be3
Rachel Carvalho <rachel.carvalho@gmail.com>
2012-06-28 23:13:10 -0300

    added icons and a basic splash

--- mobile/www/js/mobile-specific.js
+++ mobile/www/js/mobile-specific.js
@@ -2,11 +2,15 @@
 var data = offlinedata;
 
 var readyEvent = function(handler){
+  var newhandler = function(){
+    navigator.splashscreen.hide();
+    handler();
+  };
   $(document).ready(function(){
     if(!window.device)
-      $(document).bind('deviceready', handler);
+      $(document).bind('deviceready', newhandler);
     else
-      handler();
+      newhandler();
   });
 };
 
