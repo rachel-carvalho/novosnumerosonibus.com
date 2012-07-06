rachel-carvalho/novosnumerosonibus.com

commit 1bcc63bb6554eb50b68ea4320a947bcf5f45c2bd

added some visual effects

mobile/www/js/mobile-specific.js
@@ -169,7 +169,7 @@ var readyEvent = function(handler){
       if(top >= p.top)
         s.removeClass('hidden').stop().animate({top: top + 10}, 400);
       else
-        s.addClass('hidden').css('top', 0);
+        s.addClass('hidden').css('top', -50);
     });
     
     s.click(function(){
@@ -201,10 +201,39 @@ var readyEvent = function(handler){
     help = $('#help');
     var open_i = $('#open-info');
     var open_h = $('#open-help');
+    
+    var win = $(window);
+    var h1 = $('h1');
+    var win_w = win.width();
+    var info_top = h1.outerHeight(true);
+    info.css({left: win_w, top: info_top, width: win_w, height: win.height() - info_top});
+    win.resize(function(){
+      win_w = win.width();
+      info_top = h1.outerHeight(true);
+      var css = {left: win_w, top: info_top, width: win_w, height: win.height() - info_top};
+      if(!info.is(':hidden')){
+        css.left = 0;
+        css.height = '';
+      }
+      info.css(css);
+    });
 
     open_i.click(function(){
-      info.toggleClass('hidden');
-      main.toggleClass('hidden');
+      if(open_i.hasClass('close')){
+        var t = info.position().top;
+        main.removeClass('hidden');
+        info.stop().css({position: 'absolute', height: win.height() - t})
+        .animate({left: win.width()}, 'slow', function(){
+          info.addClass('hidden');
+        });
+      }
+      else{
+        info.stop().removeClass('hidden')
+        .animate({left: 0}, 'slow', function(){
+          main.addClass('hidden');
+          info.css({position: 'static', height: ''});
+        });
+      }
       open_h.toggle();
       open_i.toggleClass('close');
     });
@@ -216,7 +245,7 @@ var readyEvent = function(handler){
         search_num({type: 'prev', value: '2113'});
         update_help();
       }
-      help.toggleClass('hidden');
+      help.fadeToggle(400);
     });
 
     dbg('after info & help');

mobile/TODO
@@ -1,4 +1,5 @@
 fix header?
 enhance layout
 help: point to extras & sector (help unclear)
+async ajax
 ads

mobile/www/css/mobile.css
@@ -11,7 +11,11 @@ input {
   box-shadow: inset 0.04em 0.04em 0.12em #999;
 }
 
+h1 { margin-bottom: 0.4em; }
+
 h2 { margin: 0; padding: 0; }
+
+#main { padding-top: 0.1em; }
 
 #search { margin: auto; }
 #list-all { text-align: center; }
@@ -32,6 +36,8 @@ h2 { margin: 0; padding: 0; }
 
 #info {
   padding: 1em;
+  position: absolute;
+  background-color: #fff;
 }
 
 a {color: #0080b0; }

commit 9ed346707045ac33424e0713e6e3d86043fe9d17

fixed back button regression introduced by commit 1bcc63bb6554eb50b68ea4320a947bcf5f45c2bd

mobile/www/js/mobile-specific.js
@@ -262,9 +262,9 @@ var readyEvent = function(handler){
     
     // handles back button
     $(document).bind('backbutton', function() {
-      if(!help.hasClass('hidden'))
+      if(!help.is(':hidden'))
         open_h.click();
-      else if(!info.hasClass('hidden'))
+      else if(!info.is(':hidden'))
         open_i.click();
       else if(searches.length && (searches.length > 1 || !is_curr_search(searches[searches.length - 1])))
         search_num();