955b7abca619af2cbeac95351747f948ced235da
Rachel Carvalho <rachel.carvalho@gmail.com>
2012-07-08 23:13:17 -0300

    removing mobile www from routes

--- app.js
+++ app.js
@@ -26,7 +26,6 @@ app.configure(function(){
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(express.static(__dirname + '/public'));
-  app.use(express.static(__dirname + '/mobile/www'));
   app.use(app.router);
 });
 
